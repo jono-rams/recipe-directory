a9866d006b38f790019a978a6801f233d855fbaf
c1ph3r-dev <c1ph3r.dev@gmail.com>
2024-09-28 17:23:22 -0400

    Added recipe details page

--- src/pages/recipe/Recipe.js
+++ src/pages/recipe/Recipe.js
@@ -1,9 +1,41 @@
+import { useParams } from 'react-router-dom';
+import { useContext, useEffect, useState } from 'react';
+import { DbContext } from '../../dbContext';
+
 import './Recipe.css';
+import { doc, getDoc } from 'firebase/firestore';
 
 export default function Recipe() {
+  const { id } = useParams();
+  const db = useContext(DbContext)
+  const [recipe, setRecipe] = useState(null);
+  const [error, setError] = useState(false);
+
+  useEffect(() => {
+    const docRef = doc(db, 'recipes', id);
+    getDoc(docRef).then((snap) => {
+      if (snap.exists()) {
+        setRecipe(snap.data());
+      } else {
+        setError(true);
+      }
+    })
+  }, [db, id])
+
   return (
-    <div>
-      Recipe
+    <div className="recipe">
+      {!recipe && !error && <p className='loading'>Loading recipe details...</p>}
+      {error && <p className='error'>Error loading that recipe, it may not exist...</p>}
+      {recipe && (
+        <>
+          <h2 className='page-title'>{recipe.title}</h2>
+          <p>Takes {recipe.cookingTime} to cook.</p>
+          <ul>
+            {recipe.ingredients.map((ingredient) => <li key={ingredient}>{ingredient}</li> )}
+          </ul>
+          <p className='method'>{recipe.method}</p>
+        </>
+      )}
     </div>
   )
 }
