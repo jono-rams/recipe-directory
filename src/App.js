ece267a8ccfdfbc9d31d7c3ad3784ca66f4eb766
Jonathan Rampersad <rampersad.jonathan@gmail.com>
2024-09-28 09:03:37 -0400

    Added Firebase database (#4)

--- src/App.js
+++ src/App.js
@@ -1,9 +1,32 @@
+import { useEffect, useState } from 'react';
+import { collection, onSnapshot } from 'firebase/firestore';
 import './App.css';
 
-function App() {
+function App({ db }) {
+  // Fetch recipes from Firestore database
+  const [recipes, setRecipes] = useState([]);
+  const [recipesDb] = useState(collection(db, 'recipes'));
+
+  useEffect(() => {
+    onSnapshot(recipesDb, (snapshot) => {
+      const data = [];
+      snapshot.forEach(doc => {
+        const recipe = doc.data();
+        data.push({
+          id: doc.id,
+          title: recipe.title,
+          ingredients: recipe.ingredients,
+          method: recipe.method,
+          cookTime: recipe.cookingTime
+        });
+      });
+      setRecipes(data);
+    });
+  }, [recipesDb]);
+
   return (
     <div className="App">
-      
+      {recipes.length !== 0 && recipes.map(recipe => <p key={recipe.id}>{recipe.title}</p>)}
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,12 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
 import './index.css';
 import App from './App';
+
+let firebaseConfig = {
+  apiKey: "YOUR_FIREBASE_API_KEY",
+  authDomain: "YOUR_FIREBASE_AUTH_DOMAIN",
+  projectId: "YOUR_FIREBASE_PROJECT_ID",
+  storageBucket: "YOUR_FIREBASE_STORAGE_BUCKET",
+  messagingSenderId: "YOUR_FIREBASE_MESSAGE_SENDER_ID",
+  appId: "YOUR_FIREBASE_APP_ID",
+};
+
+const fb = initializeApp(firebaseConfig);
+const db = getFirestore(fb);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <App db={db} />
   </React.StrictMode>
 );
 
