b36859f83ac8e83810c14750544db3e20e4de4cf
c1ph3r-dev <c1ph3r.dev@gmail.com>
2024-09-29 15:42:22 -0400

    error checking

--- src/hooks/useDatabase.js
+++ src/hooks/useDatabase.js
@@ -3,5 +3,10 @@ import { DbContext } from "../context/DbContext";
 
 export const useDatabase = () => {
   const context = useContext(DbContext);
+
+  if (context === undefined) {
+    throw new Error("useDatabase must be used within a DbProvider");
+  }
+
   return context;
 };
--- src/hooks/useRecipe.js
+++ src/hooks/useRecipe.js
@@ -3,5 +3,10 @@ import { RecipesContext } from "../context/RecipeContext";
 
 export const useRecipe = () => {
   const context = useContext(RecipesContext);
+
+  if (context === undefined) {
+    throw new Error("useRecipe must be used within a RecipeProvider");
+  }
+
   return context;
 };
